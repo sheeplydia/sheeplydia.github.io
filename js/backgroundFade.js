867fa7f443f11fcfed38b6f99d406ca9a6e27922
williamDunn <willgarydunn@gmail.comm>
2021-12-13 15:28:10 -0600

    .

--- js/backgroundFade.js
+++ js/backgroundFade.js
@@ -19,7 +19,7 @@ $(window).scroll(function() {
         $('#whiteBackground').css({
             opacity: function() {
             var elementHeight = $(this).height(),
-                opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 2) + 0.1;
+                opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 2) + 0.3;
                 
             return $('#whiteBackground').css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
             }
