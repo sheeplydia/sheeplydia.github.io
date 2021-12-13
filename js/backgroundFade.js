1eecbcb2087fbf72118b0e29a89a5d5cc806eb29
williamDunn <willgarydunn@gmail.comm>
2021-12-13 15:20:14 -0600

    js try

--- js/backgroundFade.js
+++ js/backgroundFade.js
@@ -1,12 +1,38 @@
+// $(window).scroll(function() {
+//     var scrollTop = $(this).scrollTop();
+
+//     $('#whiteBackground').css({
+//         opacity: function() {
+//         var elementHeight = $(this).height(),
+//             opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 1.7) + 0.07;
+            
+//         return $('#whiteBackground').css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
+//         }
+//     });
+// });
+
+
 $(window).scroll(function() {
-                var scrollTop = $(this).scrollTop();
-    
-                $('#whiteBackground').css({
-                    opacity: function() {
-                    var elementHeight = $(this).height(),
-                        opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 1.7) + 0.07;
-                        
-                    return $('#whiteBackground').css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
-                    }
-                });
-            });
+    var scrollTop = $(this).scrollTop();
+
+    if(window.screen.height < 900) {
+        $('#whiteBackground').css({
+            opacity: function() {
+            var elementHeight = $(this).height(),
+                opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 2) + 0.1;
+                
+            return $('#whiteBackground').css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
+            }
+        });
+    }
+    else {
+        $('#whiteBackground').css({
+            opacity: function() {
+            var elementHeight = $(this).height(),
+                opacity = ((.7 - (elementHeight - scrollTop) / elementHeight) * 1.7) + 0.07;
+                
+            return $('#whiteBackground').css('background-color', 'rgba(255, 255, 255, ' + opacity + ')');
+            }
+        });
+    }
+});
